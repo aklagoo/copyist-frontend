50e1df073b916d78b169c78212d515a4450133fe
Archan Lagoo <aklagoo@gmail.com>
2021-07-28 18:31:09 -0700

    Added enzyme dependency and empty component files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 /**
  * @fileoverview Contains the entry-point code.
  */
+import React from "react";
 import "../css/App.css";
 
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    );
+  }
 }
 
 export default App;
