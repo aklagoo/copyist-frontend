50ea072345bccb854aad072142551b46a7a795a3
Archan Lagoo <aklagoo@gmail.com>
2022-01-04 15:56:00 -0700

    Completed copy implementation

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import "../css/TextBox.css";
+import Icon from '@mdi/react';
+import { mdiContentCopy, mdiContentPaste } from '@mdi/js'; 
 
 class TextBox extends React.Component {
+  constructor() {
+    super();
+    this.handleCopy = this.handleCopy.bind(this);
+  }
+
+  handleCopy() {
+    navigator.clipboard.writeText(this.props.message);
+  }
+
   render() {
     return (
       <div className='TextBox'>
         <textarea onChange={this.props.emitMessage} value={this.props.message} />
+        <div className='TextBoxButtons'>
+          <button className='ButtonCopy' onClick={this.handleCopy}><Icon path={mdiContentCopy} size={0.8}/>Copy</button>
+        </div>
       </div>
     );
     
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import "../css/Toolbar.css";
 import Icon from '@mdi/react';
 import { mdiQrcode } from '@mdi/js'; 
 
 class Toolbar extends React.Component {
   render() {
-    const style = {
-      backgroundColor: "#CCCCCC",
-      color: "rgba(0, 0, 0, 0.4)",
-    };
-
     return (
       <div className='Toolbar'>
-          <button style={style}><Icon path={mdiQrcode} size={1}/></button>
+          <button><Icon path={mdiQrcode} size={1}/></button>
       </div>
     );
   }
